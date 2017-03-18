083492223a18559ac359acca77e6d3426e4537de
elbio labra chino <elbiolabra@gmail.com>
2017-03-18 08:43:28 -0500

    2

---
+++ src/clases/JavaEscritorio.java
@@ -0,0 +1,17 @@
+
+package clases;
+
+import formularios.frmLogin;
+
+public class JavaEscritorio {
+
+     
+    public static void main(String[] args) {
+        frmLogin miLogin= new frmLogin();
+        //aca es para posiscionar el frmlogin centrado
+        miLogin.setLocationRelativeTo(null);
+        miLogin.setVisible(true);
+        
+    }
+    
+}
